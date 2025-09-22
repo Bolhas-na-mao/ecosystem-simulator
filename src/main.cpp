be2c0f95206088a1f1385deb4a25d42aa730edcf
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-09-22 17:30:42 +0000

    refactor: remove endl

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@ using namespace std;
 extern "C" {
     EMSCRIPTEN_KEEPALIVE
     void hello_ecosystem() {
-        cout << "Ecosystem Simulator initialized!" << endl;
+        cout << "Ecosystem Simulator initialized!\n";
     }
 }
 
 int main() {
-    cout << "Starting ecosystem simulator..." << endl;
+    cout << "Starting ecosystem simulator...\n";
     return 0;
 }
