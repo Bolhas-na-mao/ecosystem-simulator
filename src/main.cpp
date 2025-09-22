e0222527dc114e4250dc7aaaabf70da94193cbb1
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-09-22 17:36:31 +0000

    refactor: remove namespaces

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include <emscripten.h>
 
-using namespace std;
-
 extern "C" {
     EMSCRIPTEN_KEEPALIVE
     void hello_ecosystem() {
-        cout << "Ecosystem Simulator initialized!\n";
+        std::cout << "Ecosystem Simulator initialized!\n";
     }
 }
 
 int main() {
-    cout << "Starting ecosystem simulator...\n";
+    std::cout << "Starting ecosystem simulator...\n";
     return 0;
 }
