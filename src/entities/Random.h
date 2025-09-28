d1fc489601c91e208d97f49bb3815b093fc5a8d4
Bruno Aseff <brunoaseff2@gmail.com>
2025-09-28 13:37:16 -0300

    feat: add random number generation

---
+++ src/entities/Random.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <random>
+
+class Random {
+   public:
+    static int getNumber(int min, int max) {
+        std::uniform_int_distribution<int> dist(min, max);
+        return dist(gen);
+    }
+
+   private:
+    static std::random_device rd;
+    static std::mt19937 gen;
+};
