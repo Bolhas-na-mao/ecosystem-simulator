8e0792d0584e627d610a8214f6310663f8b590ea
Bruno Aseff <brunoaseff2@gmail.com>
2025-09-28 22:08:21 -0300

    feat: add deconstructor to avoid memory leaks

--- src/entities/World.h
+++ src/entities/World.h
@@ -52,6 +52,12 @@ class World {
         }
     };
 
+    ~World() {
+        for(int i = 0; i < 25; ++i)
+            for(int j = 0; j < 25; ++j)
+                delete cells[i][j];
+    };
+
    private:
     Being* cells[25][25];
 };
