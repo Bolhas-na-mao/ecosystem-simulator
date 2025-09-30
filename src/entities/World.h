d9dbbaca90f02264e35b00d3e660bbe4d713be15
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-09-30 17:27:48 +0000

    refactor: add size to a variable

--- src/entities/World.h
+++ src/entities/World.h
@@ -7,10 +7,13 @@
 #include "Rabbit.h"
 
 class World {
+   private:
+    int size = 25;
+
    public:
     World(int foxAmount, int rabbitAmount, int grassAmount) {
-        for(int i = 0; i < 25; i++) {
-            for(int j = 0; j < 25; j++) {
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
                 cells[i][j] = nullptr;
             }
         }
@@ -19,8 +22,8 @@ class World {
         int y = 0;
 
         for(int i = 0; i < foxAmount; i++) {
-            x = Random::getNumber(0, 24);
-            y = Random::getNumber(0, 24);
+            x = Random::getNumber(0, size - 1);
+            y = Random::getNumber(0, size - 1);
 
             if(cells[x][y] == nullptr) {
                 cells[x][y] = new Fox();
@@ -30,8 +33,8 @@ class World {
         }
 
         for(int i = 0; i < rabbitAmount; i++) {
-            x = Random::getNumber(0, 24);
-            y = Random::getNumber(0, 24);
+            x = Random::getNumber(0, size - 1);
+            y = Random::getNumber(0, size - 1);
 
             if(cells[x][y] == nullptr) {
                 cells[x][y] = new Rabbit();
@@ -57,11 +60,11 @@ class World {
     }
 
     ~World() {
-        for(int i = 0; i < 25; ++i)
-            for(int j = 0; j < 25; ++j)
+        for(int i = 0; i < size; ++i)
+            for(int j = 0; j < size; ++j)
                 delete cells[i][j];
     };
 
    private:
-    Entity* cells[25][25];
+    Entity* cells[size][size];
 };
