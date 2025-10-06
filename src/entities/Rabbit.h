81cf37da796b520c1658bafa1397cc05f7a268fc
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-10-06 18:37:47 +0000

    feat: add off spring implementation on rabbit

--- src/entities/Rabbit.h
+++ src/entities/Rabbit.h
@@ -6,4 +6,9 @@ class Rabbit : public Animal {
    public:
     Rabbit() : Animal(10) {}
     void update(World& world) override;
+
+   protected:
+    Entity* createOffspring() override {
+        return new Rabbit();
+    }
 };
