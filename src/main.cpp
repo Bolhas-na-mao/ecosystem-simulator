59121b1a9fddd271f4f8f26b1c977bbca028d4d1
Bruno Aseff <brunoaseff2@gmail.com>
2025-10-09 21:15:29 -0300

    feat: add crud operations to entities

--- src/main.cpp
+++ src/main.cpp
@@ -47,6 +47,92 @@ int getEntityAt(int x, int y) {
 }
 
 EMSCRIPTEN_KEEPALIVE
+int countEntityType(int entityType) {
+    if(gameWorld == nullptr) {
+        return 0;
+    }
+
+    int count = 0;
+    for(int i = 0; i < World::SIZE; i++) {
+        for(int j = 0; j < World::SIZE; j++) {
+            Entity* entity = gameWorld->check(i, j);
+            if(entity != nullptr) {
+                if(entityType == 1 && dynamic_cast<Fox*>(entity)) {
+                    count++;
+                } else if(entityType == 2 && dynamic_cast<Rabbit*>(entity)) {
+                    count++;
+                } else if(entityType == 3 && dynamic_cast<Grass*>(entity)) {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
+}
+
+EMSCRIPTEN_KEEPALIVE
+void placeEntity(int x, int y, int entityType) {
+    if(gameWorld == nullptr || x < 0 || x >= World::SIZE || y < 0 || y >= World::SIZE) {
+        return;
+    }
+
+    if(gameWorld->check(x, y) != nullptr) {
+        return;
+    }
+
+    Entity* newEntity = nullptr;
+    if(entityType == 1) {
+        newEntity = new Fox();
+    } else if(entityType == 2) {
+        newEntity = new Rabbit();
+    } else if(entityType == 3) {
+        newEntity = new Grass();
+    }
+
+    if(newEntity != nullptr) {
+        gameWorld->grid[x][y] = newEntity;
+        gameWorld->positions[newEntity] = {x, y};
+    }
+}
+
+EMSCRIPTEN_KEEPALIVE
+void removeEntity(int x, int y) {
+    if(gameWorld == nullptr || x < 0 || x >= World::SIZE || y < 0 || y >= World::SIZE) {
+        return;
+    }
+
+    Entity* entity = gameWorld->check(x, y);
+    if(entity != nullptr) {
+        gameWorld->positions.erase(entity);
+        delete entity;
+        gameWorld->grid[x][y] = nullptr;
+    }
+}
+
+EMSCRIPTEN_KEEPALIVE
+void replaceEntity(int x, int y, int entityType) {
+    if(gameWorld == nullptr || x < 0 || x >= World::SIZE || y < 0 || y >= World::SIZE) {
+        return;
+    }
+
+    removeEntity(x, y);
+
+    Entity* newEntity = nullptr;
+    if(entityType == 1) {
+        newEntity = new Fox();
+    } else if(entityType == 2) {
+        newEntity = new Rabbit();
+    } else if(entityType == 3) {
+        newEntity = new Grass();
+    }
+
+    if(newEntity != nullptr) {
+        gameWorld->grid[x][y] = newEntity;
+        gameWorld->positions[newEntity] = {x, y};
+    }
+}
+
+EMSCRIPTEN_KEEPALIVE
 void hello_ecosystem() {
     std::cout << "Ecosystem Simulator initialized!\n";
     logWorld();
